fd3ab25c0771d7e322062f6bfa254e68ce527f85
Brian Heilman <brian.heilman@gmail.com>
2017-06-20 23:18:09 -0400

    updating data repo

--- dist/bmoor-cache.js
+++ dist/bmoor-cache.js
@@ -5088,6 +5088,73 @@ var bmoorCache =
 	var bmoor = __webpack_require__(3),
 		Eventing = bmoor.Eventing;
 
+	function makeMask( target, seed ){
+		var mask = bmoor.isArray(target) ?
+			target.slice(0) : Object.create( target );
+		
+		// I'm being lazy
+		Object.keys(target).forEach( ( k ) => {
+			if ( bmoor.isObject(target[k]) ){
+				mask[k] = makeMask( 
+					target[k],
+					bmoor.isObject(seed) ? seed[k] : null
+				);
+			}
+		});
+
+		if ( seed ){
+			Object.keys(seed).forEach( ( k ) => {
+				if ( !mask[k] || 
+					!(bmoor.isObject(mask[k]) && bmoor.isObject(seed))
+				){
+					mask[k] = seed[k];
+				}
+			});
+		}
+
+		return mask;
+	}
+
+	function isDirty( obj ){
+		var i, c,
+			t,
+			keys = Object.keys( obj );
+
+		for( i = 0, c = keys.length; i < c; i++ ){
+			t = obj[keys[i]];
+
+			if ( !bmoor.isObject(t) || isDirty(t) ){
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	function getChanges( obj ){
+		var rtn = {},
+			valid = false;
+
+		Object.keys( obj ).forEach(function( k ){
+			var d = obj[k];
+
+			if ( bmoor.isObject(d) ){
+				d = getChanges(d);
+				if ( d ){
+					valid = true;
+					rtn[k] = d;
+				}
+			}else{
+				valid = true;
+				rtn[k] = d;
+			}
+		});
+
+		if ( valid ){
+			return rtn;
+		}
+	}
+
 	class Proxy extends Eventing {
 		constructor( obj ){
 			super();
@@ -5098,27 +5165,19 @@ var bmoorCache =
 		}
 
 		getMask( seed ){
-			var trg,
-				mask;
-
 			if ( !this.mask || seed ){
-				trg = this.getDatum();
-
-				if ( bmoor.isArray(trg) ){
-					this.mask = trg.slice(0);
-				}else{
-					this.mask = Object.create( trg );
-				}
-			}
-			
-			mask = this.mask;
-			if ( seed ){
-				Object.keys(seed).forEach(function( k ){
-					mask[k] = seed[k];
-				});
+				this.mask = makeMask( this.getDatum(), seed );
 			}
 
-			return mask;
+			return this.mask;
+		}
+
+		getChanges(){
+			return getChanges( this.mask );
+		}
+
+		isDirty(){
+			return isDirty( this.mask );
 		}
 
 		merge( delta ){
